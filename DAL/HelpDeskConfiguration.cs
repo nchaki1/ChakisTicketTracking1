e1314b61c4fda98cf5e283ef18fc531f3e44c482
Nicole Chakis <nchakis@outlook.com>
2021-07-21 14:17:06 -0400

    Implement logging

---
+++ DAL/HelpDeskConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+
+namespace ChakisTicketTracking1.DAL
+{
+    public class HelpDeskConfiguration : DbConfiguration
+    {
+        public HelpDeskConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+        }
+    }
+}
--- Global.asax.cs
+++ Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ChakisTicketTracking1.DAL;
+using System.Data.Entity.Infrastructure.Interception;
 
 namespace ChakisTicketTracking1
 {
@@ -16,6 +18,7 @@ namespace ChakisTicketTracking1
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DbInterception.Add(new HelpDeskInterceptorLogging());
         }
     }
 }
